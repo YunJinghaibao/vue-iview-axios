77ba5f0fca491d857c916b1ef773d4bb21306f87
Yunjinghaibao <353994167@qq.com>
2019-05-08 17:07:44 +0800

    11

---
+++ src/views/admin.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Button type="info">哈哈哈</Button>
+        <Row>
+            <Col span="12">
+                <Input @input="iptC"></Input>
+            </Col>
+        </Row>
+    </div>
+</template>
+
+<script>
+import MD5 from 'js-md5';
+export default {
+    name: 'admin',
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        iptC(v){
+            console.log(v);
+            let a = MD5(v);
+            console.log(a);
+        },
+    }
+}
+</script>
+
